d51489b7bd5d63936a8e568a994f8a7f99bee019
Luís Felipe <felipemmha@gmail.com>
2017-10-14 23:31:46 -0300

    git ignore changed, message to server
    Signed-off-by: Luís Felipe <felipemmha@gmail.com>

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template
-from client import Client
+#from client import Client
 
 app = Flask(__name__)
 
-data = Client()
+#data = Client()
 
 @app.route('/test')
 def name():
 
     variables = list()
 
-    variables.append("v1")
-    variables.append("v2")
-    variables.append("v3")
+    variables.append("Ola marajulha")
+    variables.append("como vai")
+    variables.append("a matilda")
 
     return render_template('index.html', variable=variables)
 
 @app.route('/')
 def index():
-    data.connect()
+    #data.connect()
     return render_template('index.html', variable=variable)
 
 if __name__ == '__main__':
